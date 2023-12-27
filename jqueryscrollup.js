3be5853c17ecb0d0c21b16a2ba894908b1f9b12c
10akanksha <154778443+10akanksha@users.noreply.github.com>
2023-12-27 13:58:09 +0530

    Update jqueryscrollup.js

--- jqueryscrollup.js
+++ jqueryscrollup.js
@@ -1,7 +1,7 @@
 /*!
  * scrollup v2.4.1
  * Url: http://markgoodyear.com/labs/scrollup/
- * Copyright (c) Mark Goodyear — @markgdyr — http://markgoodyear.com
+ * Copyright (c) Mark Goodyear â€” @markgdyr â€” http://markgoodyear.com
  * License: MIT
  */
 (function ($, window, document) {
